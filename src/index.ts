8e439f5956033afc2b28550a17415bb5fd0afc55
y-abbasi <y.abbasi.62@gmail.com>
2023-12-01 22:28:11 +0330

    init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export *  from "./services/lexical-analizer";
+export *  from "./models/token";
+export *  from "./models/context-schema";
+export *  from "./models/operation-orders";
+export *  from "./languages/ast";
+export *  from "./languages/semantic-error-analizer";
+export *  from "./languages/syntax-error-visitor";
+export *  from "./languages/terminal";
+
